3cd0ebc2911204b0aaf5c45364eaec5ee582c7b1
marvel gulane <gulanemarvel@gmail.com>
2024-07-05 20:14:35 +0100

    modify

--- src/aeonflux/app.py
+++ src/aeonflux/app.py
@@ -24,31 +24,27 @@ from aeonflux.cve_database.CVE_2024_6241 import cve_2024_6241
 from aeonflux.cve_database.CVE_2024_5447 import cve_2024_5447
 
 
-
-
 class aeonflux(toga.App):
     def startup(self):
-        
+
+        self.main_window = toga.MainWindow(title=self.formal_name,size=(400, 700))        
+        self.scroller = toga.ScrollContainer(horizontal=False, vertical=True, style=Pack(flex=1, padding=0))
+
         self.main_box = toga.Box(style=Pack(direction=COLUMN))
-        
-        name_label = toga.Label('Target:', style=Pack(padding=(0,5)),)
-                
-        self.name_input = toga.TextInput(style=Pack(flex=1))
-        
+        name_label = toga.Label('Target:', style=Pack(padding=(0,5)))                
         name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
-        
+
+        self.name_input = toga.TextInput(style=Pack(flex=1))
         button = toga.Button('Start Vulnerability Scan!', on_press=self.get_data, style=Pack(padding=5),)
-        
+
         self.main_box.add(name_box)
         self.main_box.add(button)
-        
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.scroll_container = toga.ScrollContainer(horizontal=True)
-        self.scroll_container = name_label
-        self.main_window.content = self.main_box
+        name_box.add(name_label)
+        name_box.add(self.name_input)
 
+                        
+        self.scroller.content = self.main_box
+        self.main_window.content = self.scroller
         self.main_window.show()
 
 
@@ -60,64 +56,45 @@ class aeonflux(toga.App):
                 url = 'https://'+target_domain
                 response = requests.get(url)
                 time = datetime.datetime.now()
+                time_str = now.strftime('%H:%M:%S')
+                time_str_fixed = f'{time_str:>8}'
                 
                 if response.status_code == 200:
-                    alive_message = f'[{time.hour}:{time.minute}:{time.microsecond}] target is alive in the network and can be tested.'
-                    CVE_2024_4704 = cve_2024_4704()
-                    CVE_2024_4664 = cve_2024_4664()
-                    CVE_2024_37732 = cve_2024_37732()
-                    CVE_2024_37680 = cve_2024_37680()
-                    CVE_2024_37677 = cve_2024_37677()
-                    CVE_2024_21519 = cve_2024_21519()
-                    CVE_2024_21518 = cve_2024_21518()
-                    CVE_2024_21517 = cve_2024_21517()
-                    CVE_2024_21516 = cve_2024_21516()
-                    CVE_2024_21515 = cve_2024_21515()
-                    CVE_2024_21514 = cve_2024_21514()
-                    CVE_2024_6241 = cve_2024_6241()
-                    CVE_2024_5447 = cve_2024_5447()
+                    status = f'[{time_str_fixed}] target is alive in the network.'
+                    self.main_box.add(toga.Label(status, style=Pack(background_color='#005577', color='black')))
+                    
+                    vulnerabilities = [
+                    
+                        cve_2024_4704().redirect_vulnerability(target_domain),
+                        cve_2024_4664().endpoint_verify(target_domain),
+                        cve_2024_37732().backdoor_upload(target_domain),
+                        cve_2024_37680().endpoint_xss(target_domain),
+                        cve_2024_37677().endpoint_xss(target_domain),
+                        cve_2024_21519().arbitrary_verify(target_domain),
+                        cve_2024_21518().request_xss(target_domain),
+                        cve_2024_21517().reflected_xss(target_domain),
+                        cve_2024_21516().reflected_xss(target_domain),
+                        cve_2024_21515().reflected_xss(target_domain),
+                        cve_2024_21514().sql_injection(target_domain),
+                        cve_2024_6241().sql_injection(target_domain),
+                        cve_2024_5447().endpoint_xss(target_domain),
+                    ]
 
-                    alive_message = toga.Label(alive_message, style=Pack(background_color='yellow', color='black'))
                     
-                    CVE_2024_4704 = toga.Label(CVE_2024_4704.redirect_vulnerability(target_domain))
-                    CVE_2024_4664 = toga.Label(CVE_2024_4664.endpoint_verify(target_domain))
-                    CVE_2024_37732 = toga.Label(CVE_2024_37732.backdoor_upload(target_domain))
-                    CVE_2024_37680 = toga.Label(CVE_2024_37680.endpoint_xss(target_domain))
-                    CVE_2024_37677 = toga.Label(CVE_2024_37677.endpoint_xss(target_domain))
-                    CVE_2024_21519 = toga.Label(CVE_2024_21519.arbitrary_verify(target_domain))
-                    CVE_2024_21518 = toga.Label(CVE_2024_21518.request_xss(target_domain))
-                    CVE_2024_21517 = toga.Label(CVE_2024_21517.reflected_xss(target_domain))
-                    CVE_2024_21516 = toga.Label(CVE_2024_21516.reflected_xss(target_domain))
-                    CVE_2024_21515 = toga.Label(CVE_2024_21515.reflected_xss(target_domain))
-                    CVE_2024_21514 = toga.Label(CVE_2024_21514.sql_injection(target_domain))
-                    CVE_2024_6241 = toga.Label(CVE_2024_6241.sql_injection(target_domain))
-                    CVE_2024_5447 = toga.Label(CVE_2024_5447.endpoint_xss(target_domain))
-                                        
-                    self.main_box.add(alive_message)
-                    self.main_box.add(CVE_2024_4704)
-                    self.main_box.add(CVE_2024_4664)
-                    self.main_box.add(CVE_2024_37732)
-                    self.main_box.add(CVE_2024_37680)
-                    self.main_box.add(CVE_2024_37677)
-                    self.main_box.add(CVE_2024_21519)
-                    self.main_box.add(CVE_2024_21518)
-                    self.main_box.add(CVE_2024_21517)
-                    self.main_box.add(CVE_2024_21516)
-                    self.main_box.add(CVE_2024_21515)
-                    self.main_box.add(CVE_2024_21514)
-                    self.main_box.add(CVE_2024_6241)
-                    self.main_box.add(CVE_2024_5447)
-                    
+                    for cve in vulnerabilities:
+                        if "vulnerable" in cve:
+                            self.main_box.add(toga.Label(cve, style=Pack(background_color='#33CC33', color='#FFFFFF')))
+                        else:
+                            self.main_box.add(toga.Label(cve, style=Pack(background_color='#FF0000', color='#FFFFFF')))
+                                                
                 else:
-                    down_message = f'[{time.hour}:{time.minute}:{time.microsecond}] target is not alive in the network and can not be tested!'
-                    show_values = toga.Label(down_message, style=Pack(background_color='red', color='black'))
+                    down_message = f'[{time_str_fixed}] target is not alive in the network and can not be tested!'
+                    show_values = toga.Label(down_message, style=Pack(background_color='#00FFFF', color='black'))
                     self.main_box.add(show_values)
                 
             else:
-                text = f'[{time.hour}:{time.minute}:{time.microsecond}] no target value found, cant perform the attack'
-                show_values = toga.Label(text, style=Pack(background_color='yellow', color='black'))
-                self.main_box.add(show_values)
+                text = f'[{time_str_fixed}] no target value found, cant perform the attack'
+                self.main_box.add(toga.Label(text, style=Pack(background_color='#00FFFF', color='black')))
 
 def main():
     return aeonflux()
-
